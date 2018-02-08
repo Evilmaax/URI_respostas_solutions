00f047ff3d5900da41352e25bf330c05f9c6a5b7
EvilMaax <maximilianomeyer48@gmail.com>
2018-02-08 13:18:30 -0200

    Exercícios URI

--- 1098.py
+++ 1098.py
@@ -1,10 +1,12 @@
-J1, J2, J3, I = 1.0, 2.0, 3.0, 0
+from decimal import Decimal as D
+
+J1, J2, J3, I = D(1.0), D(2.0), D(3.0), D(0)
 
 while I <= 2:
-    print('I={} J={}'.format(I, J1))
-    print('I={} J={}'.format(I, J2))
-    print('I={} J={}'.format(I, J3))
-    J1 += 0.2
-    J2 += 0.2
-    J3 += 0.2
-    I += 0.2
+    print('I={} J={}'.format(I, J1).strip('0').strip('.').replace('2.0','2').replace('1.0','1'))
+    print('I={} J={}'.format(I, J2).strip('0').strip('.').replace('2.0','2').replace('1.0','1'))
+    print('I={} J={}'.format(I, J3).strip('0').strip('.').replace('2.0','2').replace('1.0','1'))
+    J1 += D("0.2")
+    J2 += D("0.2")
+    J3 += D("0.2")
+    I += D("0.2")
