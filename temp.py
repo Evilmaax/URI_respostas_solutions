ffe8498aa6d6a46840dc18db73704ab6ee94cc12
EvilMaax <maximilianomeyer48@gmail.com>
2018-02-26 13:02:41 -0300

    Exercícios URI

--- temp.py
+++ temp.py
@@ -1,1 +1,13 @@
-print(8%2)
+M = [[0 for x in range(2)] for y in range(2)]
+
+for x in range(2):
+    for y in range(2):
+        y.append(int(input()))
+        y += 1
+    x += 1
+
+for x in range(2):
+    for y in range(2):
+        print('M[{}][{}]'.format(x, y))
+        y += 1
+    x += 1
