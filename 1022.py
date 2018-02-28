7a8309e8aa41799c4846c70f6f8bc2cce67167cc
Evilmaax <30754856+Evilmaax@users.noreply.github.com>
2018-02-27 22:54:42 -0300

    Exercícios

--- 1022.py
+++ 1022.py
@@ -1,4 +1,4 @@
-import fractions as F
+from fractions import Fraction
 N = int(input())
 
 for x in range(N):
@@ -9,33 +9,29 @@ for x in range(N):
         D1, D2 = nums[2], nums[6]
         first = int(N1)*int(D2) + int(N2)*int(D1)
         sec = int(D1)*int(D2)
-        fsx = repr(F.Fraction(first, sec)).split(', ')
-        fsx = ', '.join(fsx).split(', ')
-        print('{}/{} = {}/{}' .format(first, sec,fsx[0].strip('Fraction('), fsx[1].strip(')')))
+        f = Fraction(first, sec)
+        print('{}/{} = {}/{}' .format(first, sec, f.numerator, f.denominator))
     if nums[3] == '-':
         N1, N2 = nums[0], nums[4]
         op = nums[3]
         D1, D2 = nums[2], nums[6]
         first = int(N1)*int(D2) - int(N2)*int(D1)
         sec = int(D1)*int(D2)
-        fsx = repr(F.Fraction(first, sec)).split(', ')
-        fsx = ', '.join(fsx).split(', ')
-        print('{}/{} = {}/{}'.format(first, sec, fsx[0].strip('Fraction('), fsx[1].strip(')')))
+        f = Fraction(first, sec)
+        print('{}/{} = {}/{}'.format(first, sec, f.numerator, f.denominator))
     if nums[3] == '*':
         N1, N2 = nums[0], nums[4]
         op = nums[3]
         D1, D2 = nums[2], nums[6]
         first = int(N1)*int(N2)
         sec = int(D1)*int(D2)
-        fsx = repr(F.Fraction(first, sec)).split(', ')
-        fsx = ', '.join(fsx).split(', ')
-        print('{}/{} = {}/{}'.format(first, sec, fsx[0].strip('Fraction('), fsx[1].strip(')')))
+        f = Fraction(first, sec)
+        print('{}/{} = {}/{}'.format(first, sec, f.numerator, f.denominator))
     if nums[3] == '/':
         N1, N2 = nums[0], nums[4]
         op = nums[3]
         D1, D2 = nums[2], nums[6]
         first = int(N1) * int(D2)
         sec = int(N2) * int(D1)
-        fsx = repr(F.Fraction(first, sec)).split(', ')
-        fsx = ', '.join(fsx).split(', ')
-        print('{}/{} = {}/{}'.format(first, sec, fsx[0].strip('Fraction('), fsx[1].strip(')')))
+        f = Fraction(first, sec)
+        print('{}/{} = {}/{}'.format(first, sec, f.numerator, f.denominator))
--- temp.py
+++ temp.py
@@ -1,5 +1,3 @@
 import fractions as F
-x = repr(F.Fraction(723520/51680)).split(', ')
-z = ', '.join(x).split(', ')
-print(z[0].strip('Fraction('))
-print(z[1].strip(')'))
+x = repr(F.Fraction(723520/51680))
+print(type(x))
