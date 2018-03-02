db70effd74ec3576a62a443d68c6c554dd81fe8a
Evilmaax <30754856+Evilmaax@users.noreply.github.com>
2018-03-01 23:44:05 -0300

    Exercícios

--- temp.py
+++ temp.py
@@ -1,3 +1,23 @@
-import fractions as F
-x = repr(F.Fraction(723520/51680))
-print(type(x))
+def procura(dados,N, p):
+    for i in range(N):
+        if dados[i] == p :
+            return i+1
+
+N, Q = input().split(' ')
+N, Q = int(N), int(Q)
+
+while N!=0 and Q !=0:
+    dados = []*N
+    for i in range(N) :
+        dados.append(int(input()))
+    dados.sort()
+    for i in range(Q):
+        p =  int(input())
+        if (p in dados) :
+            print (p,'found at',procura(dados,N,p))
+        else :
+            print (p,'not found')
+
+    N = int(input())
+    Q = int(input())
+    dados[:]=[]
