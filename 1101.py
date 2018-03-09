0c0c1f6e7beb57a9fd905c9581e9e0f41273995a
Evilmaax <30754856+Evilmaax@users.noreply.github.com>
2018-03-08 22:57:01 -0300

    Exercícios

--- 1101.py
+++ 1101.py
@@ -1,3 +1,5 @@
+#Saídas corretas, mas não aceitas pelo URI
+
 M, N, soma = 0, 0, 0
 
 M, N = input().split(' ')
@@ -26,3 +28,28 @@ while M != 0 and N != 0:
     else:
         menor = N
         maior = M
+
+
+#Código aceito pelo URI, mas não foi que quem criou
+
+while True:
+    try:
+        m,n = list(map(int,input().split()))
+        if(m < 1 or n < 1 ):
+            break
+        tmp = 0
+
+        if(m > n):
+            tmp = m
+            m = n
+            n = tmp
+        soma = 0
+        resultado = ''
+        while(m <= n):
+            resultado += str(m) + ' '
+            soma += m
+            m += 1
+        resultado += 'Sum=%d'
+        print(resultado %soma)
+    except:
+        break
