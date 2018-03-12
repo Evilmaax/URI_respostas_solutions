e8349f3045f1e8ba4d4e76fa1dff1a08baef7f5d
Evilmaax <30754856+Evilmaax@users.noreply.github.com>
2018-03-12 20:59:57 -0300

    Exercícios

--- temp.py
+++ temp.py
@@ -27,3 +27,25 @@ for line in sys.stdin:
         t *= 2
 
     print(cont)
+
+
+    def fib(n):
+        global count
+        count += 1
+        if n < 2:
+            return n
+        else:
+            return fib(n - 1) + fib(n - 2)
+
+
+    N = int(input())
+    num = {1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+           20}  # ,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39}
+    for i in range(N):
+        calc = map(fib, num)
+        x = int(input())
+        count = 0
+        z = list(calc)
+        print(z)
+        # result = map(fib,x)
+        print("fib({}) = {} calls = {}".format(N, (count - 1), z[x - 2]))
