68a84221383312bd04dfe00355b920928995f548
Evilmaax <30754856+Evilmaax@users.noreply.github.com>
2018-09-05 19:49:17 -0300

    Exercícios

--- temp.py
+++ temp.py
@@ -1,18 +1,9 @@
-from functools import lru_cache
-import time
+name=str(input('Write your name: ')) #Recebe o nome
 
-@lru_cache(maxsize=39)
-def fib_com_cache(n):
-    if n < 2:
-        return n
-    return fib_com_cache(n-1) + fib_com_cache(n-2)
+def check(name): #Função para checar se o nome é elvis
+    while(name!='elvis'): #Loop que fica pedindo o nome até ser digitado o correto
+        print('You are not allowed!') #Mensagem avisando que não tem permissão
+        name=str(input('Write your name: ')) #Recebe o nome novamente
+    print('Welcome {}!'.format(name)) #Mensagem de boas vindas
 
-def fib_ate_40(funcao):
-
-    t1 = time.perf_counter()
-    for j in range(100):
-        funcao(j)
-    return time.perf_counter() - t1
-
-# Em seguida, fazemos com cache
-print('Tempo de execução com cache: {:.6f} segundos'.format(fib_ate_40(fib_com_cache)))
+check(name) #Função acima
